09cde248bfb99ce9f51eedbb8bd10c2a693fe260
EthanTransier <110576660+EthanTransier@users.noreply.github.com>
2023-11-19 17:48:47 -0700

    Finished Service Page Responsiveness

--- reveal.js
+++ reveal.js
@@ -4,14 +4,23 @@ window.addEventListener("click", function() {
     let info = document.querySelectorAll(".hidden");
     let h1 = document.querySelector(".h1");
     let pos = containerTop.getBoundingClientRect();
-
-    if (pos.top >= 0 && pos.bottom <= window.innerHeight) {
-        containerTop.classList.add("hover");
-        containerBottom.classList.add("hover"); 
-        info.forEach(x => x.classList.add("hover"));
-        h1.classList.add("hover");
-        document.getElementById("bottom").classList.add("hover");
-        document.getElementById("rightOne").classList.add("hover");
-        document.getElementById("rightTwo").classList.add("hover");
+    let start = document.getElementById('start')
+    let size = window.innerWidth
+    if(size >= 760){
+        if (pos.top >= 0 && pos.bottom <= window.innerHeight) {
+            containerTop.classList.add("hover");
+            containerBottom.classList.add("hover"); 
+            info.forEach(x => x.classList.add("hover"));
+            h1.classList.add("hover");
+            document.getElementById("bottom").classList.add("hover");
+            document.getElementById("rightOne").classList.add("hover");
+            document.getElementById("rightTwo").classList.add("hover");
+            start.classList.add('hover')
+        }
+    }else{
+        info = document.querySelectorAll('.hide')
+        console.log(info)
+        info.forEach(x => x.classList.add('reveal'))
     }
+    
 });
